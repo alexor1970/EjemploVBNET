45149f816c5538c4663d68e38dba6d2094f6db54
alexor1970 <aof1970@gmail.com>
2023-02-16 12:15:21 -0600

    Inicial

---
+++ LINQtoObjects.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LINQtoObjects
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LINQtoObjects))
+        Me.btnLINQ = New System.Windows.Forms.Button
+        Me.btnSalir = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'btnLINQ
+        '
+        Me.btnLINQ.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLINQ.Location = New System.Drawing.Point(78, 85)
+        Me.btnLINQ.Name = "btnLINQ"
+        Me.btnLINQ.Size = New System.Drawing.Size(238, 35)
+        Me.btnLINQ.TabIndex = 0
+        Me.btnLINQ.Text = "Ver Datos de Arreglo"
+        Me.btnLINQ.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Image = CType(resources.GetObject("btnSalir.Image"), System.Drawing.Image)
+        Me.btnSalir.Location = New System.Drawing.Point(287, 216)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(90, 27)
+        Me.btnSalir.TabIndex = 1
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'LINQtoObjects
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(403, 261)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnLINQ)
+        Me.Name = "LINQtoObjects"
+        Me.Text = "Ejemplo de LINQ con Objeto Array"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnLINQ As System.Windows.Forms.Button
+    Friend WithEvents btnSalir As System.Windows.Forms.Button
+
+End Class
---
+++ LINQtoObjects.vb
@@ -0,0 +1,28 @@
+﻿Public Class LINQtoObjects
+
+    Private Sub btnSalir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSalir.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnLINQ_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLINQ.Click
+        ' Declaramos la matriz y la inicializamos con valores
+        Dim material() As String = {"mesa", "reloj", "libro", "pluma", "borrador", "pelota", "botella"}
+        ' Ejecutamos LINQ para obtener los datos buscados
+        ' Concretamente, buscamos los materiales que empiezan por "b"
+        Dim materialBuscado = From busqueda In material _
+        Where busqueda.StartsWith("b") _
+        Select busqueda  ' es una coleccion de datos del arreglo creada aqui mismo
+        ' En materialBuscado tendremos toda la seleccirealizada, por(lo)
+        ' que deberemos recorrerla para extraer los elementos encontrados()
+        Dim resultado As String = ""
+        For Each elementos In materialBuscado
+            resultado &= elementos & vbCrLf  ' salta al final de la linea a la siguiente y vuelve a imprimir en pantalla 
+        Next
+        ' Mostramos la información
+        MessageBox.Show(resultado)
+    End Sub
+
+    Private Sub LINQtoObjects_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
